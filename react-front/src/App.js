9375a83f3a63c0a74c530731c0ad5866a4295523
Ivan Garcia <igarcia@appen.com>
2022-11-25 00:06:16 -0600

    Added react app with docker

--- react-front/src/App.js
+++ react-front/src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+// mock data
+import data from "./data.json";
+// components
+import Header from "./Header";
+import ToDoList from "./ToDoList";
+import ToDoForm from './ToDoForm';
+// groomet styling
+import { Grommet, Box } from 'grommet';
+
+const theme = {
+  global: {
+    font: {
+      family: 'Roboto',
+      size: '18px',
+      height: '20px',
+    },
+  },
+};
+
 
 function App() {
+  
+  const [ toDoList, setToDoList ] = useState(data);
+
+  const handleToggle = (id) => {
+    let mapped = toDoList.map(task => {
+      return task.id === Number(id) ? { ...task, complete: !task.complete } : { ...task};
+    });
+    setToDoList(mapped);
+  }
+
+  const handleFilter = () => {
+    let filtered = toDoList.filter(task => {
+      return !task.complete;
+    });
+    setToDoList(filtered);
+  }
+
+  const addTask = (userInput ) => {
+    let copy = [...toDoList];
+    copy = [...copy, { id: toDoList.length + 1, task: userInput, complete: false }];
+    setToDoList(copy);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grommet theme={theme}>
+      <Box align="center" background="neutral-3" pad="medium">
+        <Header />
+        <ToDoList toDoList={toDoList} handleToggle={handleToggle} handleFilter={handleFilter}/>
+        <ToDoForm addTask={addTask}/>
+      </Box>
+    </Grommet>
   );
 }
 
